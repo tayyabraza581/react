fde876c674e3b09ed8fb6f34d83ac247029e40d7
tayyabraza581 <tr7508235@gmail.com>
2025-09-30 05:57:02 -0700

    change the color

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,14 +1,24 @@
-
-
-
 function App() {
- 
-
   return (
-  <>
-  <h4 className="bg-dark text-info p-4 m-5 justify-content-center align-items-center" >tayyab raza</h4>
-  </>
-  )
+    <>
+     
+        <div
+          className=" bg-dark mx-auto shadow rounded px-4 py-3 text-white"
+          style={{ maxWidth: "42rem" }}
+        >
+          <h1 className="text-center mb-4 mt-2 fw-bold fs-3">
+            Manage Your Todos
+          </h1>
+
+          <div className="mb-4">{/* Todo form goes here */}</div>
+
+          <div className="d-flex flex-wrap gap-2">
+            {/* Loop and Add TodoItem here */}
+          </div>
+        </div>
+      {/* </div> */}
+    </>
+  );
 }
 
-export default App
+export default App;
